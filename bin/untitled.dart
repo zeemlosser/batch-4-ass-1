fcd3ca050dfae38988769c38fd8eebaacf755815
zeemlooser <myhpc0153@gmail.com>
2023-08-04 20:53:30 +0600

    first commit

---
+++ bin/untitled.dart
@@ -0,0 +1,12 @@
+
+void main() {
+  const int a = 7;
+  const int b = 3;
+
+  print("a = $a, b = $b");
+  print("Addition: $a + $b = ${a + b}");
+  print("Subtraction: $a - $b = ${a - b}");
+  print("Multiplication: $a * $b = ${a * b}");
+  print("Division: $a / $b = ${a / b}");
+  print("Modulus: $a % $b = ${a % b}");
+}
